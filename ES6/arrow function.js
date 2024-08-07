b8b4e7d7cf0900a1f2f4b79e196083fe74d34cd4
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-07 15:43:11 +0530

    Create arrow function.js

---
+++ ES6/arrow function.js
@@ -0,0 +1,12 @@
+//Arrow function
+function test(){
+    console.log("Hello World");
+}
+
+let test=function(){
+    console.log("Hello World");
+}
+
+let test=() =>{
+    console.log("Hello World");
+}
