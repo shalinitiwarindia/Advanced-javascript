e9be1f6f8b631c56a990fe23a816eb210b0d3c7e
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-08 20:54:26 +0530

    Create script.js

---
+++ ES6/YouTube functioning clone/script.js
@@ -0,0 +1,36 @@
+
+const API="AIzaSyD85F2Vt3-ykW-f8N0r3OlBeDRHpbteHFM";
+
+const searchVideos=async()=> {
+    try {
+        const q=document.getElementById("query").value;
+        const res = await fetch(
+            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=${q}%202&key=${API}`
+        );
+        const data=await res.json();
+        append(data.items);
+        
+    }catch(err){
+        console.log(err);
+    }
+};
+
+const append=(videos) => {
+    let show_videos=document.getElementById("show_videos");
+    show_videos.innerHTML=null;
+    videos.forEach(({id:{videoId},snippet:{title}})=>{
+        let div=document.createElement("div");
+        let iframe=document.createElement("iframe");
+        iframe.src=`https://www.youtube.com/embed/${videoId}`;
+        iframe.width="100%";
+        iframe.height="100%";
+        iframe.allow="fullscreen";
+
+        let name=document.createElement("h5");
+
+        name.innerText=title;
+        div.append(iframe,name);
+        show_videos.append(div);
+    });
+};
+
