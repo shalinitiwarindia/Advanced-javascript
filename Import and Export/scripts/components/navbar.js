243bc670083fe400d26dfefaca6d9a82d7843327
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-15 14:07:02 +0530

    Update navbar.js

--- Import and Export/scripts/components/navbar.js
+++ Import and Export/scripts/components/navbar.js
@@ -8,12 +8,7 @@ function navbar() {
         <a href="jewelery.html">Jewellery</a>
         </h3>
         <h3>
-        <a href="electronics.html">Electronics</a>
-        </h3>
-        <h3>
-        <a href="signup.html">SignUp</a>
-        </h3>
-        <h3>
+      
         <a href="login.html">Login</a>
         </h3>
         </div> 
