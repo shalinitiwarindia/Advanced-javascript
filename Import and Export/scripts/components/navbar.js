dddd3f00b943e5fc3bd88edac2014f7762c3666f
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-15 14:07:14 +0530

    Update navbar.js

--- Import and Export/scripts/components/navbar.js
+++ Import and Export/scripts/components/navbar.js
@@ -4,10 +4,7 @@ function navbar() {
         <a href="/">Home</a>
         </h3>
         <div id="options">
-        <h3>
-        <a href="jewelery.html">Jewellery</a>
-        </h3>
-        <h3>
+      
       
         <a href="login.html">Login</a>
         </h3>
