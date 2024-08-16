1630882adea22ec3c257fc4996e0d6b6a4b8ccc3
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-08-16 20:36:52 +0530

    Create Object & Array destructuring.js

---
+++ ES6/Object & Array destructuring.js
@@ -0,0 +1,20 @@
+var person = {}
+person.name = 'karthik';
+person.id = 'sm_090';
+person.location = 'India';
+// creating property one by one
+
+const name = person.name
+const id = person.id
+// extracting property one by one
+
+// creating multiple properties together by using object literal notation
+var person = {
+    name:"karthik",
+    id:"sm_090",
+    location:"India"
+}
+
+var name = person.name
+var id = person.id
+var location = person.location
